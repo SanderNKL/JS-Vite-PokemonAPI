61fc61c9d9b9f77eb2e393702d192551381ac27a
SanderNKL <sander.lastad@gmail.com>
2025-08-07 09:16:14 +0200

    init commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { setupPokemonForm } from './modules/setupPokemonForm.js'
+
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Pokedex</h1>
+    <form id='searchPokemonForm'>
+        <input
+            id='searchPokemonInput'
+            type='text'
+            name='searchPokemonInput'
+        >
+        <input type='submit'>
+    </form>
+  </div>
+`
+
+setupPokemonForm(document.querySelector("#searchPokemonForm"));
